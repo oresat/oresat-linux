a77354431596793d9bc09c4ea00a20973eb920f2
Ryan Medick <rmedick@pdx.edu>
2019-08-11 14:18:12 -0700

    pydbus example now have signals too

--- CAN/dbus_examples/pydbus-example-server.py
+++ CAN/dbus_examples/pydbus-example-server.py
@@ -1,34 +1,33 @@
 #!/usr/bin/env python3
 
-# Python DBUS Test Server
-# runs until the Quit() method is called via DBUS
-
-from gi.repository import GLib
+from pydbus.generic import signal
 from pydbus import SessionBus
+from gi.repository import GLib
+import random
 
-loop = GLib.MainLoop()
-
-class MyDBUSService(object):
+class Server_XML(object):
     """
-        <node>
-            <interface name='net.lew21.pydbus.ClientServerExample'>
-                <method name='GetData'>
-                    <arg type='s' name='test_string' direction='out'/>
-                    <arg type='d' name='test_float' direction='out'/>
-                    <arg type='i' name='test_int' direction='out'/>
-                </method>
-                <method name='Command'>
-                    <arg type='s' name='command' direction='in'/>
-                    <arg type='s' name='response' direction='out'/>
-		</method>
-		<method name='Quit'/>
-            </interface>
-        </node>
+    Server_XML definition.
+    Emit / Publish a signal that is a random integer every second 
+    type='i' for integer. 
     """
-
-    def GetData(self):
-        """returns its data"""
-        return ("Hello, World!", 1.2, 10)
+    dbus = """
+    <node>
+        <interface name="org.example.project_1.server_1">
+            <method name='Command'>
+                <arg type='s' name='command' direction='in'/>
+                <arg type='s' name='response' direction='out'/>
+            </method>
+	    <method name='Quit'/>
+            <signal name="app_1_signal">
+                <arg type='s'/>
+                <arg type='i'/>
+                <arg type='d'/>
+            </signal>
+        </interface>
+    </node>
+    """
+    app_1_signal = signal()
 
     def Command(self, s):
         """to pass command from C3 card"""
@@ -39,6 +38,28 @@ class MyDBUSService(object):
         """removes this object from the DBUS connection and exits"""
         loop.quit()
 
-bus = SessionBus()
-bus.publish("net.lew21.pydbus.ClientServerExample", MyDBUSService())
-loop.run()
+def repeating_timer():
+    """Generate random integer between 0 and 100 and emit over Session D-Bus
+    return True to keep the GLib timer calling this function once a second."""
+    test_string = "HelloWorld"
+    random_integer = random.randint(0,100)
+    random_double = random.uniform(0.0,10.0)
+    print(test_string, random_integer, random_double)
+    emit.app_1_signal(test_string, random_integer, random_double)
+    return True
+
+if __name__=="__main__":
+    loop = GLib.MainLoop()
+    interface_name = "org.example.project_1.server_1"
+    bus = SessionBus()
+    # Setup server to emit signals over the DBus
+    emit = Server_XML()
+    bus.publish(interface_name, emit)
+    # Setup repeating timer
+    GLib.timeout_add_seconds(interval=1, function=repeating_timer) 
+    # Run loop with graceful ctrl C exiting.
+    try:
+        loop.run()
+    except KeyboardInterrupt as e:
+        loop.quit()
+        print("\nExit by Control C")
