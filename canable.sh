f57a4f292b89aaec7a753d8cb0199634f23432b0
Ryan Medick <rmedick@pdx.edu>
2019-08-13 17:57:55 -0700

    updated canable.sh to look for sudo

--- canable.sh
+++ canable.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+user=`id -u`
+
+if [ $user -ne 0 ]
+  then echo "Run as root"
+  exit
+fi
+
 killall -q slcand
 slcand -o -c -f -s6 /dev/serial/by-id/usb-CANtact_CANtact_dev_00000000001A-if00 can0
 ip link set can0 up
