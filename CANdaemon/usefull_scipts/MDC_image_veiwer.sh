12f5c23d164433c3d565e57364b1fb3f227dd816
ryanpdx <rmedick@pdx.edu>
2019-10-31 09:24:02 -0700

    added a bash version of MDC imager veiwer

---
+++ CANdaemon/usefull_scipts/MDC_image_veiwer.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+a=1
+while [[ $a -eq 1 ]]; do
+    fileAvalible=`./../src/app_star_tracker/canopencomm 0x12 r 0x3002 3 u8`
+    fileAvalible="${fileAvalible:4}"
+    
+    b=`echo $fileAvalible | grep "ERROR"`
+    
+    if [[ -z $b ]]; then
+        fileName=`./../src/app_star_tracker/canopencomm 0x12 r 0x3002 1 d`
+        fileName="${fileName:3}" # remove starting 3 chars
+        fileName="${fileName::-2}" # remove last 2 chars
+        fileName=`echo $fileName | xxd -r -p` # conv2ert hex string into ascii string
+
+        fileData=`./../src/app_star_tracker/canopencomm 0x12 r 0x3002 2 d`
+        fileData="${fileData:3}" # remove starting 3 chars
+        fileData="\x"`echo $fileData | sed 's/ /\\\x/g'` # remove all spaces
+
+        echo -e $fileData > $fileName # save to file
+        pidof feh && pkill feh # this will check if there's a process called feh and kill it
+        feh "$fileName" &
+    fi
+    
+    sleep 60 
+done
