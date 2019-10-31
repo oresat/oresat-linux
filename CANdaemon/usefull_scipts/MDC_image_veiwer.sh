1300343acf6a241ad051f809f5a342ce77aa2519
ryanpdx <rmedick@pdx.edu>
2019-10-31 12:16:20 -0700

    added image file types check to MDC viewer

--- CANdaemon/usefull_scipts/MDC_image_veiwer.sh
+++ CANdaemon/usefull_scipts/MDC_image_veiwer.sh
@@ -14,16 +14,21 @@ while [[ $a -eq 1 ]]; do
         fileName="${fileName:3}" # remove starting 3 chars
         fileName="${fileName::-2}" # remove last 2 chars
         fileName=`echo $fileName | xxd -r -p` # conv2ert hex string into ascii string
+        
+        # check for image file types
+        if [[ *".bmp" == $fileName ]] || [[ *".jpg" == $fileName ]] || [[ *".png" == $fileName ]]; then 
 
-        fileData=`./../src/app_star_tracker/canopencomm 0x12 r 0x3002 2 d`
-        fileData="${fileData:3}" # remove starting 3 chars
-        fileData="\x"`echo $fileData | sed 's/ /\\\x/g'` # remove all spaces
+            fileData=`./../src/app_star_tracker/canopencomm 0x12 r 0x3002 2 d`
+            fileData="${fileData:3}" # remove starting 3 chars
+            fileData="\x"`echo $fileData | sed 's/ /\\\x/g'` # remove all spaces
 
-        echo -e $fileData > $fileName # save to file
+            echo -e $fileData > lastest_image # save to file
 
-        if [[ $firstImage -ne 1 ]]; then
-            feh "$fileName" -R 1 &
-            firstImage=1
+            # start feh on the first image
+            if [[ $firstImage -ne 1 ]]; then
+                feh latest_image -R 1 &
+                firstImage=1
+            fi
         fi
     fi
     
