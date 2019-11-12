38a60c269173e0baabcb870303ddfdd0aed6c10b
ryanpdx <rmedick@pdx.edu>
2019-11-12 09:42:49 -0800

    change font color and size

--- CANdaemon/usefull_scipts/MDC_image_veiwer.sh
+++ CANdaemon/usefull_scipts/MDC_image_veiwer.sh
@@ -5,17 +5,17 @@ outputImageName="latest_image"
 
 a=1
 while [[ $a -eq 1 ]]; do
-    temp=`./../src/app_star_tracker/canopencomm 0x12 r 0x3100 0 u8` # capture new image
+    temp=`./../CANopenComm/canopencomm 0x12 r 0x3100 0 u8` # capture new image
     
     sleep 5
 
-    fileAvalible=`./../src/app_star_tracker/canopencomm 0x12 r 0x3002 3 u8`
+    fileAvalible=`./../CANopenComm/canopencomm 0x12 r 0x3002 3 u8`
     fileAvalible="${fileAvalible:4}"
     
     b=`echo $fileAvalible | grep "ERROR"`
     
     if [[ -z $b ]]; then
-        fileName=`./../src/app_star_tracker/canopencomm 0x12 r 0x3002 1 d`
+        fileName=`./../CANopenComm/canopencomm 0x12 r 0x3002 1 d`
         fileName="${fileName:3}" # remove starting 3 chars
         fileName="${fileName::-2}" # remove last 2 chars
         fileName=`echo $fileName | xxd -r -p` # conv2ert hex string into ascii string
@@ -23,11 +23,11 @@ while [[ $a -eq 1 ]]; do
         # check for image file types
         if [[ $fileName =~ .*".bmp" ]] || [[ $fileName =~ .*".jpg" ]] || [[ $fileName =~ .*".jpeg" ]]; then 
 
-            fileData=`./../src/app_star_tracker/canopencomm 0x12 r 0x3002 2 d`
+            fileData=`./../CANopenComm/canopencomm 0x12 r 0x3002 2 d`
             
             outputImageName="/tmp/output.bin"
             
-            eval `convert $outputImageName -font Noto-Sans-Black  -fill blue -pointsize 10 -annotate +10+10 "$fileName $(date)" $outputImageName`
+            eval `convert $outputImageName -font Noto-Sans-Black  -fill white -pointsize 100 -annotate +10+10 "$fileName $(date)" $outputImageName`
 
             # start feh on the first image
             if [[ $firstImage -ne 1 ]]; then
@@ -37,5 +37,5 @@ while [[ $a -eq 1 ]]; do
         fi
     fi
     
-    sleep 900 
+    sleep 300 
 done
