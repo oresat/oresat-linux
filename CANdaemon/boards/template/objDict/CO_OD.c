565865220def81b0cd76628f4041b6607f7a1fc6
ryanpdx <rmedick@pdx.edu>
2020-01-17 11:35:19 -0800

    changed systemd_ODF to writeonly

--- CANdaemon/boards/template/objDict/CO_OD.c
+++ CANdaemon/boards/template/objDict/CO_OD.c
@@ -339,8 +339,8 @@ struct sCO_OD_EEPROM CO_OD_EEPROM = {
 
 /*0x3000*/ const CO_OD_entryRecord_t OD_record3000[3] = {
            {(void*)&CO_OD_RAM.systemdControl.maxSubIndex, 0x06, 0x1 },
-           {(void*)0, 0x0e, 0x0 },
-           {(void*)0, 0x0e, 0x0 },
+           {(void*)0, 0x0a, 0x0 },
+           {(void*)0, 0x0a, 0x0 },
 };
 
 /*0x3001*/ const CO_OD_entryRecord_t OD_record3001[4] = {
@@ -428,7 +428,7 @@ const CO_OD_entry_t CO_OD[53] = {
 {0x2108, 0x01, 0x8e, 2, (void*)&CO_OD_RAM.temperature[0]},
 {0x2109, 0x01, 0x8e, 2, (void*)&CO_OD_RAM.voltage[0]},
 {0x2130, 0x03, 0x00, 0, (void*)&OD_record2130},
-{0x3000, 0x02, 0x00, 1, (void*)&OD_record3000},
+{0x3000, 0x02, 0x00, 0, (void*)&OD_record3000},
 {0x3001, 0x03, 0x00, 0, (void*)&OD_record3001},
 {0x3002, 0x7f, 0x0e, 0, (void*)0},
 {0x3003, 0x08, 0x00, 0, (void*)&OD_record3003},
