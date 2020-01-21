580938824c728d8b05f14223d66cadfd641fb2c4
ryanpdx <rmedick@pdx.edu>
2020-01-21 13:40:46 -0800

    added verion and architecture to package deb name

--- updater_onboard/make-deb-package.sh
+++ updater_onboard/make-deb-package.sh
@@ -3,37 +3,42 @@
 PKG_NAME="oresat-linux-updater"
 
 # remove old build dir, if it exists
-rm -rf $PKG_NAME
+rm -rf $PKG_NAME"*"
+
+VERSION=`grep -i version src/deb-pkg-files/control | cut -d ":" -f 2 | sed 's/ //g'`
+ARCHITECTURE=`grep -i architecture src/deb-pkg-files/control | cut -d ":" -f 2 | sed 's/ //g'`
+
+BUILD_DIR=$PKG_NAME"_"$VERSION"_"$ARCHITECTURE
 
 # Create the build dir
-mkdir $PKG_NAME
+mkdir $BUILD_DIR
 
 # Create the DEBIAN dir
-DEBIAN_DIR=$PKG_NAME"/DEBIAN/"
+DEBIAN_DIR=$BUILD_DIR"/DEBIAN/"
 mkdir $DEBIAN_DIR
 cp src/deb-pkg-files/* $DEBIAN_DIR
 
 # create debian binary
-echo "2.0" > $PKG_NAME"/debian-binary"
+echo "2.0" > $BUILD_DIR"/debian-binary"
 
 # systemd dbus 
-DBUS_PATH=$PKG_NAME"/usr/share/dbus-1/system.d/"
+DBUS_PATH=$BUILD_DIR"/usr/share/dbus-1/system.d/"
 mkdir -p $DBUS_PATH
 cp src/org.oresat.linux.updater.conf $DBUS_PATH
 
 # systemd daemon service file
-DAEMON_PATH=$PKG_NAME"/lib/systemd/system/"
+DAEMON_PATH=$BUILD_DIR"/lib/systemd/system/"
 mkdir -p $DAEMON_PATH
 cp src/oresat-linux-updater.service $DAEMON_PATH
 
 # source code
-SOURCE_PATH=$PKG_NAME"/usr/share/"$PKG_NAME
+SOURCE_PATH=$BUILD_DIR"/usr/share/"$PKG_NAME
 mkdir -p $SOURCE_PATH
 cp src/updater.py $SOURCE_PATH
 cp src/updater_daemon.py $SOURCE_PATH
 
 #make md5sums file
-cd $PKG_NAME
+cd $BUILD_DIR
 find . -type f ! -regex '.*?debian-binary.*' ! -regex '.*?DEBIAN.*' -printf '%P ' | xargs md5sum > DEBIAN/md5sums
 cd -
 
