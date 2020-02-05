17627c9acc1767327929b2a5221656c501cb6a72
dmitri-mcguckin <dmitri.mcguckin26@gmail.com>
2020-02-05 14:03:50 -0800

    added some ncurses stuff

--- CANcli/driver.py
+++ CANcli/driver.py
@@ -1,27 +1,59 @@
 #!/usr/bin/env python3
+import curses
 from canard.hw import socketcan
 from frame_data import FrameData
 from frame_table import FrameTable
-from ftfutils import log, Mode
 
-def main():
+def main(argv):
+    table_size = 16
+
     # Table Setup
-    hb_table = FrameTable("Heartbeat", 4)
-    sdo_table = FrameTable("RDO's", 4)
-    misc_table = FrameTable("Miscellaneous", 4)
+    tables = [
+        FrameTable("Heartbeat", table_size),
+        FrameTable("RDO's", table_size),
+        FrameTable("Miscellaneous", table_size)
+    ]
 
     # Open the socket to vCAN
     dev = socketcan.SocketCanDev('vcan0')
     dev.start()
 
+    # Open the standard output
+    stdscr = curses.initscr()
+    curses.noecho()
+    curses.cbreak()
+
     while True:
         new_frame = FrameData(dev.recv())
         id = new_frame.id
 
-        if(id >= 700 and id < 800): hb_table.add(new_frame)
-        elif(id >= 500 and id < 700): sdo_table.add(new_frame)
-        else: misc_table.add(new_frame)
+        if(id >= 700 and id < 800): tables[0].add(new_frame)
+        elif(id >= 500 and id < 700): tables[1].add(new_frame)
+        else: tables[2].add(new_frame)
 
-        log(Mode.DEBUG, str(hb_table) + "\n" + str(sdo_table) + "\n" + str(misc_table))
+        for i, table in enumerate(tables):
 
-if __name__ == "__main__": main()
+            stdscr.addstr((i * table_size) + i * 2, 0, str(table.name) + ": (" + str(len(table.table)) + ")")
+            stdscr.addstr("\n\tId:\tStatus\tData:")
+            for k in sorted(table.table.keys()):
+                frame = table.table[k]
+                if(frame.is_stale()): status = "STALE"
+                elif(frame.is_dead()): status = "DEAD"
+                else: status = "ALIVE"
+                stdscr.addstr("\n\t" + str(frame.id)
+                                     + "\t" + status
+                                     + "\t[")
+                for val in frame.data:
+                    stdscr.addstr(" " + str(hex(val)))
+                stdscr.addstr(" ]")
+
+
+        stdscr.refresh()
+
+    # Close the standard input
+    curses.nocbreak()
+    stdscr.keypad(False)
+    curses.echo()
+    curses.endwin()
+
+if __name__ == "__main__": curses.wrapper(main)
