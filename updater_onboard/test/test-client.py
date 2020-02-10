3d1b75662b776de837de4ea6caa81ac8f16d8be0
ryanpdx <rmedick@pdx.edu>
2020-02-09 18:51:13 -0800

    fixed bug in test update script

--- updater_onboard/test/test-client.py
+++ updater_onboard/test/test-client.py
@@ -6,7 +6,7 @@ from pydbus import SystemBus
 from gi.repository import GLib
 
 
-INTERFACE_NAME = "org.OreSat.Updater"
+INTERFACE_NAME = "org.OreSat.LinuxUpdater"
 EXAMPLE_UPDATE_DIR = "/home/debian/oresat-linux/updater_onboard/test/example-updates/"
 
 
@@ -15,16 +15,10 @@ if __name__=="__main__":
     loop = GLib.MainLoop() # only used by server
     the_object = bus.get(INTERFACE_NAME)
 
-    print(the_object.ComputerName)
-    print(the_object.Status)
-    the_object.ComputerName = "startracker"
-    print(the_object.ComputerName)
-
-
     update_file = EXAMPLE_UPDATE_DIR + "update-2020-01-03-08-57.tar.gz"
 
-    print("Calling AddUpdateFile method with {}".format(update_file))
-    reply = the_object.AddUpdateFile(update_file)
+    print("Calling AddArchiveFile method with {}".format(update_file))
+    reply = the_object.AddArchiveFile(update_file)
     print("Returned: {}\n".format(reply))
 
     print("Calling StartUpdate\n")
