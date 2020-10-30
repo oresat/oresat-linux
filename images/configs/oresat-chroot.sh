70a6b8eee8e5cccfff978f7c24378c870002ddeb
ryanpdx <rmedick@pdx.edu>
2020-10-29 23:32:19 -0700

    swap to debian 10.6, simplified chroot script, fixed uEnv PRU path

--- images/configs/oresat-chroot.sh
+++ images/configs/oresat-chroot.sh
@@ -1,70 +1,9 @@
 #!/bin/sh -e
 
-export LC_ALL=C
-
-#contains: rfs_username, release_date
-if [ -f /etc/rcn-ee.conf ] ; then
-	. /etc/rcn-ee.conf
-fi
-
-if [ -f /etc/oib.project ] ; then
-	. /etc/oib.project
-fi
-
-export HOME=/home/${rfs_username}
-export USER=${rfs_username}
-export USERNAME=${rfs_username}
-
-echo "env: [`env`]"
-
-is_this_qemu () {
-	unset warn_qemu_will_fail
-	if [ -f /usr/bin/qemu-arm-static ] ; then
-		warn_qemu_will_fail=1
-	fi
-}
-
-qemu_warning () {
-	if [ "${warn_qemu_will_fail}" ] ; then
-		echo "Log: (chroot) Warning, qemu can fail here... (run on real armv7l hardware for production images)"
-		echo "Log: (chroot): [${qemu_command}]"
-	fi
-}
-
-git_clone () {
-	mkdir -p ${git_target_dir} || true
-	qemu_command="git clone ${git_repo} ${git_target_dir} --depth 1 || true"
-	qemu_warning
-	git clone ${git_repo} ${git_target_dir} --depth 1 || true
-	chown -R 1000:1000 ${git_target_dir}
-	sync
-	echo "${git_target_dir} : ${git_repo}" >> /opt/source/list.txt
-}
-
-git_clone_branch () {
-	mkdir -p ${git_target_dir} || true
-	qemu_command="git clone -b ${git_branch} ${git_repo} ${git_target_dir} --depth 1 || true"
-	qemu_warning
-	git clone -b ${git_branch} ${git_repo} ${git_target_dir} --depth 1 || true
-	chown -R 1000:1000 ${git_target_dir}
-	sync
-	echo "${git_target_dir} : ${git_repo}" >> /opt/source/list.txt
-}
-
-git_clone_full () {
-	mkdir -p ${git_target_dir} || true
-	qemu_command="git clone ${git_repo} ${git_target_dir} || true"
-	qemu_warning
-	git clone ${git_repo} ${git_target_dir} || true
-	chown -R 1000:1000 ${git_target_dir}
-	sync
-	echo "${git_target_dir} : ${git_repo}" >> /opt/source/list.txt
-}
-
 ##############################################################################
-# QMEU check
+# disable root login
 
-is_this_qemu
+echo "PermitRootLogin prohibit-password" >> /etc/ssh/sshd_config
 
 ##############################################################################
 # set default boot power mode
@@ -80,14 +19,14 @@ echo "" >> /etc/securetty
 echo "#USB Gadget Serial Port" >> /etc/securetty
 echo "ttyGS0" >> /etc/securetty
 
-#cat > "/etc/default/bb-boot" <<-__EOF__
-#USB_CONFIGURATION=enable
-#USB_NETWORK_DISABLED="yes"
-#__EOF__
+cat > "/etc/default/bb-boot" <<-__EOF__
+USB_CONFIGURATION=enable
+USB_NETWORK_DISABLED="yes"
+__EOF__
 
-#echo "g_ether" > /etc/modules-load.d/g_ether.conf
-#HOST_ADDR=`dmesg | grep "usb0: HOST MAC" | cut -d " " -f 8`
-#echo "options g_ether host_addr=$HOST_ADDR" > /etc/modprobe.d/g_ether.conf
+echo "g_ether" > /etc/modules-load.d/g_ether.conf
+HOST_ADDR=`dmesg | grep "usb0: HOST MAC" | cut -d " " -f 8`
+echo "options g_ether host_addr=$HOST_ADDR" > /etc/modprobe.d/g_ether.conf
 
 ##############################################################################
 # setup systemd-networkd
@@ -100,7 +39,6 @@ Name=can1
 BitRate=1M
 __EOF__
 
-
 cat > "/etc/systemd/network/20-wired.network" <<-__EOF__
 [Match]
 Name=usb0
@@ -110,6 +48,7 @@ Address=168.192.6.2/24
 Gateway=168.192.6.1
 DNS=168.192.6.1
 DNS=8.8.8.8
+DNS=8.8.4.4
 __EOF__
 
 # enable systemd-networkd on boot
@@ -117,6 +56,11 @@ systemctl enable systemd-networkd
 systemctl enable systemd-resolved
 
 ##############################################################################
-# remove internet packages required during build and other packages installed
+# remove internet packages required during build
 
-apt -y remove git git-man curl wget rsync ca-certificates nano
+apt -y purge git git-man curl wget rsync
+
+##############################################################################
+# remove things we don't want that are install
+
+apt -y purge rtl8723bu-modules-`uname -r` rtl8821cu-modules-`uname -r` nano nano-tiny
