e5d41d3c5cdc6b69f2c5589590222dfcba41e329
ryanpdx <rmedick@pdx.edu>
2020-11-22 14:53:33 -0800

    added some oresat packages to startracker conf

--- images/configs/apt-conf.sh
+++ images/configs/apt-conf.sh
@@ -3,3 +3,7 @@ cat > "/etc/apt/apt.conf" <<-__EOF__
 APT::Install-Suggests "0";
 APT::Install-Recommends "0";
 __EOF__
+
+# add oresat debian repo
+echo "deb [trusted=yes] https://debian.oresat.org/packages ./" >> /etc/apt/sources.list
+apt update
