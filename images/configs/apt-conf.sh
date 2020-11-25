cfccb3e01f572683eda29e326289fcfe43e450d3
ryanpdx <rmedick@pdx.edu>
2020-11-24 22:12:05 -0800

    early chroot read from .project file

--- images/configs/apt-conf.sh
+++ images/configs/apt-conf.sh
@@ -1,9 +1,11 @@
+tempdir=`grep -i tempdir= .project | cut -d "=" -f 2`
+
 # to remove install installing recommended and suggestes packages
-cat > "/etc/apt/apt.conf" <<-__EOF__
+cat > "${tempdir}/etc/apt/apt.conf" <<-__EOF__
 APT::Install-Suggests "0";
 APT::Install-Recommends "0";
 __EOF__
 
 # add oresat debian repo
-echo "deb [trusted=yes] https://debian.oresat.org/packages ./" >> /etc/apt/sources.list
+echo "deb [trusted=yes] https://debian.oresat.org/packages ./" >> ${tempdir}/etc/apt/sources.list
 apt update
