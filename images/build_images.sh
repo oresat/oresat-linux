b44af6be39bebfa1a6f1d610c8fc37faee7a158d
ryanpdx <rmedick@pdx.edu>
2021-04-21 09:11:09 -0700

    added olm-configs during to images

--- images/build_images.sh
+++ images/build_images.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]; then
-    echo "No arguments supplied"
+if [ $# -ne 1 ]; then
+    echo "Invalid arguments"
+    echo ""
     echo "./build_images <BOARD>"
+    echo ""
     echo "where <BOARD> can be:"
     echo "  cfc"
     echo "  dxwifi"
@@ -11,6 +13,9 @@ if [ $# -eq 0 ]; then
     echo "  star-tracker"
     exit 1
 fi
+
+NODE_ID=`grep "^$1=" node_ids.txt | cut -d "=" -f 2`
+echo $NODE_ID
 
 BOARD="oresat-"$1
 echo $BOARD
@@ -23,6 +28,19 @@ fi
 cp ./configs/*.conf ./image-builder/configs/
 cp ./configs/*.sh ./image-builder/target/chroot/
 cp ./configs/*.txt ./image-builder/target/boot/
+
+echo "\
+
+##############################################################################
+echo \"adding OLM config\"
+
+cat >> \"/etc/oresat-linux-manager.conf\" <<-__EOF__
+[Bus]
+Name=can1
+
+[Node]
+ID=$NODE_ID
+__EOF__" >> image-builder/target/chroot/oresat-chroot.sh
 
 # override setup_sdcard script
 cp ./setup_sdcard.sh ./image-builder/tools/
