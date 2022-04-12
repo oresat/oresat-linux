ea4b350d637c52cc6cb6864d830c74dfdfa231b7
ryanpdx <rmedick@pdx.edu>
2022-04-11 20:22:25 -0700

    replace beagleboard systemd-networkd configs

--- image_builder/chroot_scripts/oresat-chroot.sh
+++ image_builder/chroot_scripts/oresat-chroot.sh
@@ -20,6 +20,43 @@ __EOF__
 fi
 
 ##############################################################################
+echo "replace BeagleBoard's systemd-networkd config"
+
+# disable networking script
+cat > "/etc/default/bb-boot" <<-__EOF__
+USB_CONFIGURATION=disable
+USB_NETWORK_DISABLED_yes
+__EOF__
+
+# remove BeagleBoard's configs
+rm /etc/systemd/network/*
+
+cat > "/etc/systemd/network/10-can.network" <<-__EOF__
+[Match]
+Name=can*
+
+[Link]
+RequiredForOnline=no
+
+[CAN]
+BitRate=1M
+__EOF__
+
+# usb0 (for Window host)
+# usb1 (for *nix host)
+# eth0 (for BeagleBone Black ethernet port)
+cat > "/etc/systemd/network/20-wired.network" <<-__EOF__
+[Match]
+Name=usb0 usb1 eth0
+
+[Link]
+RequiredForOnline=no
+
+[Network]
+DHCP=yes
+__EOF__
+
+##############################################################################
 echo "remove internet packages required during build on flight images"
 
 if [ $HOSTNAME != "oresat-dev" ]; then
