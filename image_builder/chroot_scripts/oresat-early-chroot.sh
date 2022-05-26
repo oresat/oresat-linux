af6c7433f373c95cede9de6430b6e6b7bebe88fa
ryanpdx <rmedick@pdx.edu>
2022-05-25 17:03:21 -0700

    device tree overlays are added during image build again

--- image_builder/chroot_scripts/oresat-early-chroot.sh
+++ image_builder/chroot_scripts/oresat-early-chroot.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -e
 
+tempdir=`grep -i tempdir= .project | cut -d "=" -f 2 | sed 's/\"//g'`
+
 # to remove install installing recommended and suggestes packages
 cat > "${tempdir}/etc/apt/apt.conf" <<-__EOF__
 APT::Install-Suggests "0";
@@ -9,3 +11,6 @@ __EOF__
 # add oresat debian repo
 #echo "deb [trusted=yes] https://packages.oresat.org/debian bullseye main" >> ${tempdir}/etc/apt/sources.list
 #apt update
+
+# add all oresat dtbo
+cp ../device_trees_overlays/*.dtbo $tempdir/lib/firmware/
