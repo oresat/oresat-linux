3896b19fb5fa7143ae19be7b7f6e173fef6298a6
ryanpdx <rmedick@protonmail.com>
2023-01-28 17:29:54 -0800

    minor fix to oresat-chroot script

--- image_builder/chroot_scripts/oresat-chroot.sh
+++ image_builder/chroot_scripts/oresat-chroot.sh
@@ -50,7 +50,7 @@ http {
 
     location / {
       proxy_pass http://127.0.0.1:8000/;
-      proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;A
+      proxy_set_heade X-Forwarded-For \$proxy_add_x_forwarded_for;
       proxy_set_header X-Forwarded-Proto \$scheme;
       proxy_set_header X-Forwarded-Host \$host;
       proxy_set_header X-Forwarded-Prefix /;
