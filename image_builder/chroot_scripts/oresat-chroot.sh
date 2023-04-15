d6c9e1c17925d4e6d5f7954f8969e5600b55e648
ryanpdx <rmedick@protonmail.com>
2023-04-15 09:47:02 -0700

    fix misspelling in nginx configs

--- image_builder/chroot_scripts/oresat-chroot.sh
+++ image_builder/chroot_scripts/oresat-chroot.sh
@@ -48,7 +48,7 @@ http {
 
     location / {
       proxy_pass http://127.0.0.1:8000/;
-      proxy_set_heade X-Forwarded-For \$proxy_add_x_forwarded_for;
+      proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
       proxy_set_header X-Forwarded-Proto \$scheme;
       proxy_set_header X-Forwarded-Host \$host;
       proxy_set_header X-Forwarded-Prefix /;
