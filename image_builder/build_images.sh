4ecff09cecc8c7329bc0b5af115008b65c4fb95b
Ryan Medick <rmedick@protonmail.com>
2024-01-30 18:21:44 -0800

    remove version from build_images scripts

--- image_builder/build_images.sh
+++ image_builder/build_images.sh
@@ -2,27 +2,15 @@
 
 list="c3 cfc dev dxwifi gps star-tracker"
 
-help_message() {
-    echo "./build_images.sh <BOARD> <VERION>"
+if [[ ! $list =~ (^|[[:space:]])$1($|[[:space:]]) ]]; then
+    echo "Invalid board argument"
+    echo ""
+    echo "./build_images.sh <BOARD>"
     echo ""
     echo "where <BOARD> can be:"
     for i in $list; do
         echo "  $i"
     done
-    echo ""
-    echo "where <VERION> is optional but must match a device tree version if "
-    echo "added. If not added the latest version will be used. Not used on dev"
-    echo "images."
-    echo ""
-    echo "Examples:"
-    echo "  ./build_images.sh gps"
-    echo "  ./build_images.sh c3 v1.0"
-}
-
-if [[ ! $list =~ (^|[[:space:]])$1($|[[:space:]]) ]]; then
-    echo "Invalid board argument"
-    echo ""
-    help_message
     exit 1
 fi
 
@@ -30,25 +18,6 @@ BOARD="oresat-"$1
 DATE=`date "+%F"`
 NAME="$BOARD-$DATE"
 SIZE="2gb"
-version=$2
-
-if [ $BOARD == "oresat-dev" ]; then
-    dtb=""
-    version=""
-elif [ -z "$version" ]; then
-    dtss=(`ls ../device_trees/$BOARD-v*.dts`)
-    dts=${dtss[-1]}
-    dtb=../device_trees/`basename -- "$dts" .dts`.dtb
-    version=`echo $dtb | awk -F "-" '{print $NF}' | cut -d . -f 1 | sed 's/_/./g'`
-else
-    dtb=$BOARD-`echo $version | sed 's/./_/'`.dtb
-    if [ ! -f ../device_trees/$dtb]; then
-        echo "Invalid version argument"
-        echo ""
-        help_message
-        exit 1
-    fi
-fi
 
 if [ ! -d image-builder ]; then
     git clone https://github.com/beagleboard/image-builder
