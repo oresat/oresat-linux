7d1f5b9385d435996a423e5700de3adfe88bba7b
Chad Coates <chroco@gmail.com>
2024-08-31 14:21:22 -0600

    change device

--- uboot/write_uboot.sh
+++ uboot/write_uboot.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+DEVICE=sdb
+
 umount /media/chad/rootfs
-dd if=u-boot-dtb.img of=/dev/sdc count=4 seek=1 bs=384k
-dd if=MLO of=/dev/sdc count=2 seek=1 bs=128k
+dd if=u-boot-dtb.img of=/dev/${DEVICE} count=4 seek=1 bs=384k
+dd if=MLO of=/dev/${DEVICE} count=2 seek=1 bs=128k
