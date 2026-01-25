299ad17739a85f225fa01eb6cc5f5f847db1ef0c
Cullen Sharp <cullen.sharp100@gmail.com>
2026-01-25 13:53:33 -0800

    Fixes mDNS for usb0
    `20-wired.network` isn't being used by `networkd` to configure the usb0
    interface, so I broke the configuration into multiple files that should
    achieve the same thing.
    
    Forked beagle board imager to include python build tool upgrades in
    chroot scripts for compatibility with newer packaging workflow.

--- image_builder/chroot_scripts/oresat-chroot.sh
+++ image_builder/chroot_scripts/oresat-chroot.sh
@@ -1,38 +1,38 @@
 #!/bin/sh -e
 
-HOSTNAME=`cat /etc/hostname`
+HOSTNAME=$(cat /etc/hostname)
 
 ##############################################################################
 echo "disable root login"
 
-echo "PermitRootLogin prohibit-password" >> /etc/ssh/sshd_config
+echo "PermitRootLogin prohibit-password" >>/etc/ssh/sshd_config
 
 ##############################################################################
 echo "enable g_ether (ethernet over usb)"
 
 MAC_ADDR_BASE="60:64:05:f9:0d"
 if [ $HOSTNAME == "oresat-c3" ]; then
-MAC_ADDR="$MAC_ADDR_BASE:10"
+  MAC_ADDR="$MAC_ADDR_BASE:10"
 elif [ $HOSTNAME == "oresat-cfc" ]; then
-MAC_ADDR="$MAC_ADDR_BASE:20"
+  MAC_ADDR="$MAC_ADDR_BASE:20"
 elif [ $HOSTNAME == "oresat-dxwifi" ]; then
-MAC_ADDR="$MAC_ADDR_BASE:30"
+  MAC_ADDR="$MAC_ADDR_BASE:30"
 elif [ $HOSTNAME == "oresat-gps" ]; then
-MAC_ADDR="$MAC_ADDR_BASE:40"
+  MAC_ADDR="$MAC_ADDR_BASE:40"
 elif [ $HOSTNAME == "oresat-star-tracker" ]; then
-MAC_ADDR="$MAC_ADDR_BASE:50"
+  MAC_ADDR="$MAC_ADDR_BASE:50"
 else
-MAC_ADDR="$MAC_ADDR_BASE:f0"
+  MAC_ADDR="$MAC_ADDR_BASE:f0"
 fi
 
-echo "g_ether" > /etc/modules-load.d/g_ether.conf
-echo "options g_ether host_addr=$HOST_ADDR" > /etc/modprobe.d/g_ether.conf
+echo "g_ether" >/etc/modules-load.d/g_ether.conf
+echo "options g_ether host_addr=$HOST_ADDR" >/etc/modprobe.d/g_ether.conf
 
 ##############################################################################
 echo "add OreSat OLAF app daemon"
 
 # add config
-cat > "/etc/systemd/system/"$HOSTNAME"d.service" <<-__EOF__
+cat >"/etc/systemd/system/"$HOSTNAME"d.service" <<-__EOF__
 [Unit]
 Description=OreSat Linux App
 
@@ -53,13 +53,13 @@ __EOF__
 # enable daemon
 systemctl daemon-reload
 if [ $HOSTNAME != "oresat-dev" ] && [ $HOSTNAME != "oresat-generic" ]; then
-systemctl enable $HOSTNAME"d.service"
+  systemctl enable $HOSTNAME"d.service"
 fi
 
 ##############################################################################
 echo "add growparts oneshot daemon"
 
-cat > "/etc/systemd/system/grow-partition.service" <<-__EOF__
+cat >"/etc/systemd/system/grow-partition.service" <<-__EOF__
 [Unit]
 Description=Grow active root partition
 
@@ -80,7 +80,7 @@ systemctl enable grow-partition.service
 ##############################################################################
 echo "setup and configure nginx for OreSat OLAF app"
 
-cat > "/etc/nginx/nginx.conf" <<-__EOF__
+cat >"/etc/nginx/nginx.conf" <<-__EOF__
 events {
   worker_connections 768;
 }
@@ -106,27 +106,42 @@ systemctl enable nginx.service
 ##############################################################################
 echo "enable SPIDEV kernel module at boot"
 
-cat > "/etc/modules-load.d/spidev.conf" <<-__EOF__
+cat >"/etc/modules-load.d/spidev.conf" <<-__EOF__
 spidev
 __EOF__
 
 ##############################################################################
 echo "add systemd-networkd configs"
 
-cat > "/etc/systemd/network/10-usb0.link" <<-__EOF__
+cat >"/etc/systemd/network/10-usb0.link" <<-__EOF__
 [Match]
 OriginalName=usb0
 
 [Link]
+RequiredForOnline=no
 MACAddress=$MAC_ADDR
 __EOF__
 
-cat > "/etc/systemd/network/20-wired.network" <<-__EOF__
+cat >"/etc/systemd/network/10-usb0.network" <<-__EOF__
 [Match]
-Name=usb0 eth0
+Name=usb0
+
+[Network]
+DHCP=yes
+MulticastDNS=yes
+__EOF__
+
+cat >"/etc/systemd/network/10-eth0.link" <<-__EOF__
+[Match]
+Name=eth0
 
 [Link]
-RequiredForOnline=no
+RequiredForOnline=no 
+__EOF__
+
+cat >"/etc/systemd/network/10-eth0.network" <<-__EOF__
+[Match]
+name=eth0
 
 [Network]
 DHCP=yes
@@ -145,35 +160,35 @@ python3 -m pip install --force-reinstall --no-cache-dir --no-binary pyyaml pyyam
 ##############################################################################
 echo "add oresat device trees to /boot"
 
-dtb_dir=`ls -d /boot/dtbs/*`
+dtb_dir=$(ls -d /boot/dtbs/*)
 mv /tmp/*.dtb $dtb_dir
 chmod 755 $dtb_dir/oresat*
 
 if [ $HOSTNAME != "oresat-dev" ] && [ $HOSTNAME != "oresat-generic" ]; then
-echo "replacing pocketbeagle dt with latest custom card dt"
-DT_PATH=`ls -d /boot/dtbs/*`
-DT=`ls $DT_PATH/$HOSTNAME-*.dtb | tail -1`
-mv $DT_PATH/am335x-pocketbeagle.dtb $DT_PATH/am335x-pocketbeagle.dtb-orig
-ln -s $DT $DT_PATH/am335x-pocketbeagle.dtb
+  echo "replacing pocketbeagle dt with latest custom card dt"
+  DT_PATH=$(ls -d /boot/dtbs/*)
+  DT=$(ls $DT_PATH/$HOSTNAME-*.dtb | tail -1)
+  mv $DT_PATH/am335x-pocketbeagle.dtb $DT_PATH/am335x-pocketbeagle.dtb-orig
+  ln -s $DT $DT_PATH/am335x-pocketbeagle.dtb
 fi
 
 ##############################################################################
 # Card unique changes
 
 if [ $HOSTNAME = "oresat-star-tracker" ]; then
-LOST_WHL="lost-0.0.0-py3-none-any.whl"
-wget https://packages.oresat.org/python/$LOST_WHL
-pip3 install $LOST_WHL
-rm -f $LOST_WHL
+  LOST_WHL="lost-0.0.0-py3-none-any.whl"
+  wget https://packages.oresat.org/python/$LOST_WHL
+  pip3 install $LOST_WHL
+  rm -f $LOST_WHL
 fi
 
 ##############################################################################
 # Flight images only
 
 if [ $HOSTNAME != "oresat-dev" ]; then
-echo "remove internet packages required during build"
-apt -y purge git git-man curl wget rsync
+  echo "remove internet packages required during build"
+  apt -y purge git git-man curl wget rsync
 
-echo "disable timesyncd"
-systemctl disable systemd-timesyncd.service
+  echo "disable timesyncd"
+  systemctl disable systemd-timesyncd.service
 fi
