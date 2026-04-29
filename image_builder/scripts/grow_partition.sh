8b42081d229badb1da00cb65c0ea2d54865210f6
Cullen Sharp <cullen.sharp100@gmail.com>
2026-04-28 19:25:34 -0700

    Fix typo

--- image_builder/scripts/grow_partition.sh
+++ image_builder/scripts/grow_partition.sh
@@ -1,18 +1,18 @@
 #!/bin/bash -e
 
 if [ $# -eq 0 ]; then
-    mounted_root_partition=$(df --output=source / | grep /dev)
+  mounted_root_partition=$(df --output=source /)
+  mounted_root_partition=${mounted_root_partition#Filesystem}
 elif [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
-    echo "Will grow the root parition to use all available space"
-    echo "Usage: $0 <root parition>"
-    echo "If no root parition is given, the mounted root parition will be expaned"
-    echo "example: $0 /dev/mmcblk0p2"
-    exit 0
+  echo "Will grow the root parition to use all available space"
+  echo "Usage: $0 <root parition>"
+  echo "If no root parition is given, the mounted root parition will be expaned"
+  echo "example: $0 /dev/mmcblk0p2"
+  exit 0
 else
-    mounted_root_partition=$1
+  mounted_root_partition=$1
 fi
 
-mounted_root_partition=$(basename "$mounted_root_partition")
 growpart_args=${mounted_root_partition//p/" "}
-sudo growpart "/dev/$growpart_args"
-sudo resize2fs "/dev/$mounted_root_partition"
+sudo growpart "${growpart_args}"
+sudo resize2fs "${mounted_root_partition}"
